f526fd3ce04c3ecc26cd1c3b670e046e67979687
fadimata tillman <fadi.tillman@gmail.com>
2020-04-05 01:53:40 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES
+("Mr. Chocolate",2,"M","brown","nice",1),
+("Rowdy",3,"M","brown","nice",0),
+("Tabitha",4,"F","brown","nice",1),
+("Sergeant Brown",5,"M","brown","nice",0),
+("Melissa",6,"F","brown","nice",1),
+("Wendy",7,"F","brown","nice",0),
+("Bobo",8,"M","brown","nice",1),
+(NULL,9,NULL,NULL,NULL,1);
